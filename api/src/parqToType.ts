72da2d6582c730cea3cde86b979a168dbb655f7e
jdetok <jdeko17@gmail.com>
2026-08-30 01:04:39 -0500

    first working code in /api, read public parquet file,  add type mirroring parquet data

---
+++ api/src/parqToType.ts
@@ -0,0 +1,7 @@
+// run with: npx tsx ./api/src/parqToType.ts
+import { findRecentParquetInDir, formatParquetColsAsType, getParquet, readParquetFile } from './utils/parquet';
+
+// reads local parquet if it exists, otherwise fetches from UC OSPO s3 bucket
+const parqBuf = (await readParquetFile(await findRecentParquetInDir()) || await getParquet());
+
+console.log(await formatParquetColsAsType(parqBuf)); 
